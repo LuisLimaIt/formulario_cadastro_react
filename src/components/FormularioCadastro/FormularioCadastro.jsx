0462c17429a4ba0f422a40ed3217787b7e76c7ea
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2022-01-13 12:11:36 -0300

    Primeiro commit do projeto

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function FormularioCadastro() {
+    return (
+        <form>
+            <label>Nome</label>
+            <input type="text"/>
+            
+            <label>Sobrenome</label>
+            <input type="text"/>
+           
+            <label>CPF</label>
+            <input type="text"/>
+           
+            <label>Promoções</label>
+            <input type="checkbox"/>
+           
+            <label>Novidades</label>
+            <input type="checkbox"/>
+
+            <button type='submit'>Cadastrar</button>
+
+        </form>
+    )
+}
